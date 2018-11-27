34f1b7c8cfd198e8e73bcf55aa0c04ec657f1d45
Kimmo Puputti <git@kimmo.kpuputti.fi>
2018-11-27 10:44:07 +0200

    Add initial structure

---
+++ src/pages/guides.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Layout from '../components/layout';
+
+export default () => {
+  return (
+    <Layout title="How-to Guides">
+      <h1>How-to Guides</h1>
+    </Layout>
+  );
+};
---
+++ src/pages/tutorials.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Layout from '../components/layout';
+
+export default () => {
+  return (
+    <Layout title="Tutorials">
+      <h1>Tutorials</h1>
+    </Layout>
+  );
+};
