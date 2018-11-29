9b2beb595f7f142a7b97f9e45afd917d2e607c53
Kimmo Puputti <git@kimmo.kpuputti.fi>
2018-11-29 13:58:41 +0200

    Add responsive grid to the landing page sections

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,35 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import { baselineSpacing, H1, H2, P, Ul, Li } from '../brand-components';
+import {
+  baselineSpacing,
+  baselineBreakpoint,
+  H1,
+  H2,
+  P,
+  Ul,
+  Li,
+} from '../brand-components';
 import { SingleColumnLayout } from '../layouts';
 import { Link } from '../components';
 
-const Paragraph = styled(P)`
-  margin: ${baselineSpacing}px 0;
+const Grid = styled.div`
+  margin-top: ${2 * baselineSpacing}px;
+  display: grid;
+  grid-row-gap: ${2 * baselineSpacing}px;
+  grid-column-gap: ${2 * baselineSpacing}px;
+
+  @media (min-width: ${baselineBreakpoint}px) {
+    grid-template-columns: 1fr 1fr;
+    grid-template-rows: auto auto;
+  }
 `;
 
-const LinksUl = styled(Ul).attrs({
-  withBullets: true,
-})`
+const Intro = styled(P)`
+  margin-top: ${baselineSpacing}px;
+`;
+
+const Paragraph = styled(P)`
   margin: ${baselineSpacing}px 0;
 `;
 
@@ -19,73 +37,75 @@ const IndexPage = () => {
   return (
     <SingleColumnLayout>
       <H1>Sharetribe Flex developer documentation</H1>
-      <Paragraph>Welcome to the docs. This is the intro.</Paragraph>
-      <section>
-        <H2>
-          <Link neutral to="/tutorials">
-            Tutorials
-          </Link>
-        </H2>
-        <Paragraph>
-          Get started in learning about the product with hands-on tutorials for
-          developers.
-        </Paragraph>
-        <LinksUl>
-          <Li>Getting Started</Li>
-          <Li>
-            <Link to="/tutorials">All tutorials</Link>
-          </Li>
-        </LinksUl>
-      </section>
-      <section>
-        <H2>
-          <Link neutral to="/guides">
-            How-to Guides
-          </Link>
-        </H2>
-        <Paragraph>Specific step-by-step guides for solving </Paragraph>
-        <LinksUl>
-          <Li>How to...</Li>
-          <Li>
-            <Link to="/guides">All guides</Link>
-          </Li>
-        </LinksUl>
-      </section>
-      <section>
-        <H2>
-          <Link neutral to="/references">
-            Reference
-          </Link>
-        </H2>
-        <Paragraph>Technical reference to the tooling.</Paragraph>
-        <LinksUl>
-          <Li>API Reference</Li>
-          <Li>
-            <Link to="/references">All reference</Link>
-          </Li>
-        </LinksUl>
-      </section>
-      <section>
-        <H2>
-          <Link neutral to="/background">
-            Background
-          </Link>
-        </H2>
-        <Paragraph>
-          Explanations and background information for important concepts and
-          design decisions behind the product.
-        </Paragraph>
-        <LinksUl>
-          <Li>Important concepts</Li>
-          <Li>Sharetribe Flex architecture</Li>
-          <Li>
-            <Link to="/background">All background articles</Link>
-          </Li>
-          <Li>
-            <Link to="/styleguide">Styleguide</Link>
-          </Li>
-        </LinksUl>
-      </section>
+      <Intro>Welcome to the docs. This is the intro.</Intro>
+      <Grid>
+        <section>
+          <H2>
+            <Link neutral to="/tutorials">
+              Tutorials
+            </Link>
+          </H2>
+          <Paragraph>
+            Get started in learning about the product with hands-on tutorials
+            for developers.
+          </Paragraph>
+          <Ul withBullets>
+            <Li>Getting Started</Li>
+            <Li>
+              <Link to="/tutorials">All tutorials</Link>
+            </Li>
+          </Ul>
+        </section>
+        <section>
+          <H2>
+            <Link neutral to="/guides">
+              How-to Guides
+            </Link>
+          </H2>
+          <Paragraph>Specific step-by-step guides for solving </Paragraph>
+          <Ul withBullets>
+            <Li>How to...</Li>
+            <Li>
+              <Link to="/guides">All guides</Link>
+            </Li>
+          </Ul>
+        </section>
+        <section>
+          <H2>
+            <Link neutral to="/references">
+              Reference
+            </Link>
+          </H2>
+          <Paragraph>Technical reference to the tooling.</Paragraph>
+          <Ul withBullets>
+            <Li>API Reference</Li>
+            <Li>
+              <Link to="/references">All reference</Link>
+            </Li>
+          </Ul>
+        </section>
+        <section>
+          <H2>
+            <Link neutral to="/background">
+              Background
+            </Link>
+          </H2>
+          <Paragraph>
+            Explanations and background information for important concepts and
+            design decisions behind the product.
+          </Paragraph>
+          <Ul withBullets>
+            <Li>Important concepts</Li>
+            <Li>Sharetribe Flex architecture</Li>
+            <Li>
+              <Link to="/background">All background articles</Link>
+            </Li>
+            <Li>
+              <Link to="/styleguide">Styleguide</Link>
+            </Li>
+          </Ul>
+        </section>
+      </Grid>
     </SingleColumnLayout>
   );
 };
