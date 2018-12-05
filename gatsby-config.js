7fcf33f67a8211e955a286480f55f8db93072c66
Kimmo Puputti <git@kimmo.kpuputti.fi>
2018-12-05 14:00:09 +0200

    Update manifest metadata and icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -39,13 +39,13 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-manifest',
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
+        name: 'Sharetribe Flex documentation',
+        short_name: 'Flex',
         start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
+        background_color: '#f9f9f9',
+        theme_color: '#f9f9f9',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+        icon: 'src/images/logo.png', // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
