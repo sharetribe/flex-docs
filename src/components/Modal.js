2c17cc06767e74711ea93231c6bd2d4f3515909a
Kimmo Puputti <git@kimmo.kpuputti.fi>
2018-12-05 20:37:49 +0200

    Add Modal component

---
+++ src/components/Modal.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import { default as ReactModal } from 'react-modal';
+import styled from 'styled-components';
+
+import {
+  baselineSmall,
+  baselineLarge,
+  baselineSpacing,
+  baselineBreakpoint,
+  P,
+} from '../brand-components';
+
+const CloseButton = styled.button`
+  position: absolute;
+  top: 0;
+  right: 0;
+  width: ${10 * baselineSmall}px;
+  height: ${10 * baselineSmall}px;
+
+  @media (min-width: ${baselineBreakpoint}px) {
+    width: ${10 * baselineLarge}px;
+    height: ${10 * baselineLarge}px;
+  }
+`;
+
+class Modal extends Component {
+  componentDidMount() {
+    ReactModal.setAppElement('#___gatsby');
+  }
+  render() {
+    const { isOpen, contentLabel, onClose, children } = this.props;
+    return (
+      <ReactModal
+        isOpen={isOpen}
+        contentLabel={contentLabel}
+        onRequestClose={onClose}
+        style={{
+          content: {
+            top: 0,
+            right: 0,
+            bottom: 0,
+            left: 0,
+            border: 'none',
+            padding: 0,
+          },
+        }}
+      >
+        {children}
+        <CloseButton onClick={onClose}>
+          <P>Close</P>
+        </CloseButton>
+      </ReactModal>
+    );
+  }
+}
+
+export default Modal;
--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,7 @@
 // Components that don't import other components from this
 // index. These should not create any circular dependencies.
 export { default as Link } from './Link';
+export { default as Modal } from './Modal';
 
 // Components that import other components from this index. These
 // might create circular dependencies. Changing the order of these
