f48888b1bac5ae3d939bf8edd7b2a6e9049dc51f
Kimmo Puputti <git@kimmo.kpuputti.fi>
2018-12-18 14:31:23 +0200

    Add Footer component

--- src/components/index.js
+++ src/components/index.js
@@ -2,6 +2,7 @@
 // index. These should not create any circular dependencies.
 export { default as Link } from './Link';
 export { default as Modal } from './Modal';
+export { default as Footer } from './Footer';
 
 // Components that import other components from this index. These
 // might create circular dependencies. Changing the order of these
