44670b7176cbbc30d2815b6beaa22c8764fb4b57
Kimmo Puputti <git@kimmo.kpuputti.fi>
2018-12-28 14:01:07 +0200

    Set up sitemap and robots.txt files

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,38 @@
+const { NODE_ENV, NODE_VERSION } = process.env;
+const NETLIFY_SITE_URL =
+  process.env.URL ||
+  `http://localhost:${NODE_ENV === 'development' ? 8000 : 9000}`;
+const NETLIFY_DEPLOY_URL = process.env.DEPLOY_PRIME_URL || NETLIFY_SITE_URL;
+const NETLIFY_ENV = process.env.CONTEXT || NODE_ENV;
+
+const isNetlifyProduction = NETLIFY_ENV === 'production';
+const siteUrl = isNetlifyProduction ? NETLIFY_SITE_URL : NETLIFY_DEPLOY_URL;
+
+const siteTitle = 'Sharetribe Flex documentation';
+
+console.log({
+  NODE_ENV,
+  NODE_VERSION,
+  NETLIFY_SITE_URL,
+  NETLIFY_DEPLOY_URL,
+  NETLIFY_ENV,
+  isNetlifyProduction,
+  siteUrl,
+});
+
 module.exports = {
+  // ================ Site metadata ================
+  //
   siteMetadata: {
-    title: 'Sharetribe Flex documentation',
+    title: siteTitle,
+    siteUrl,
   },
   plugins: [
-    // Header metadata
+    // ================ Header metadata ================
+    //
     'gatsby-plugin-react-helmet',
 
-    // Markdown and images
+    // ================ Markdown and images ================
     //
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
@@ -33,14 +59,16 @@ module.exports = {
       },
     },
 
-    // Styles
+    // ================ Styles ================
+    //
     'gatsby-plugin-styled-components',
 
-    // Miscellaneous
+    // ================ Manifest ================
+    //
     {
       resolve: 'gatsby-plugin-manifest',
       options: {
-        name: 'Sharetribe Flex documentation',
+        name: siteTitle,
         short_name: 'Flex',
         start_url: '/',
         background_color: '#f9f9f9',
@@ -49,6 +77,49 @@ module.exports = {
         icon: 'src/images/logo.png', // This path is relative to the root of the site.
       },
     },
+
+    // ================ Sitemap ================
+    //
+    {
+      // NOTE: sitemap is NOT built with dev server
+
+      resolve: 'gatsby-plugin-sitemap',
+      options: {
+        exclude: ['/styleguide'],
+      },
+    },
+
+    // ================ Robots.txt ================
+    //
+    {
+      // NOTE: robots.txt file is NOT built with dev server
+
+      resolve: 'gatsby-plugin-robots-txt',
+      options: {
+        resolveEnv: () => NETLIFY_ENV,
+        env: {
+          production: {
+            policy: [{ userAgent: '*', disallow: [] }],
+          },
+
+          // Disallow indexing anything on branch deploys and deploy previews.
+          //
+          // See: https://github.com/mdreizin/gatsby-plugin-robots-txt/tree/v1.3.0#netlify
+          //
+          'branch-deploy': {
+            policy: [{ userAgent: '*', disallow: ['/'] }],
+            sitemap: null,
+            host: null,
+          },
+          'deploy-preview': {
+            policy: [{ userAgent: '*', disallow: ['/'] }],
+            sitemap: null,
+            host: null,
+          },
+        },
+      },
+    },
+
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     // 'gatsby-plugin-offline',
