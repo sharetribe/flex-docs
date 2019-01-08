dc84004f5e656e6c7bc8f82cdf799e36a02a313c
Kimmo Puputti <git@kimmo.kpuputti.fi>
2019-01-08 13:46:47 +0200

    Add thanks page where feedback form redirects

---
+++ src/pages/thanks-for-the-feedback.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { ThanksForTheFeedbackPage } from '../components';
+
+/**
+ * This is the page that the feedback form redirects to when
+ * finished. The feedback form link is in the end of the landing page.
+ *
+ * NOTE: Do not change the page URL!
+ */
+export default () => {
+  return <ThanksForTheFeedbackPage />;
+};
