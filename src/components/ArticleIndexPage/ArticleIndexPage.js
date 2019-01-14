77d0441f96a336f365b15d34084836661f905bee
Kimmo Puputti <git@kimmo.kpuputti.fi>
2019-01-14 11:27:42 +0200

    Add grid styles to article index page

--- src/components/ArticleIndexPage/ArticleIndexPage.js
+++ src/components/ArticleIndexPage/ArticleIndexPage.js
@@ -7,62 +7,66 @@ import {
   baselineLarge,
   P,
   H1,
-  Hr,
 } from '../../brand-components';
-import { MainLayout, Breadcrumb, UiText } from '../../components';
+import { grid } from '../../config';
+import { MainLayout, UiText } from '../../components';
 import ArticleIndex from './ArticleIndex';
 
 const Content = styled.div`
-  padding-left: ${props => props.theme.contentPaddingSmall}px;
-  padding-right: ${props => props.theme.contentPaddingSmall}px;
-  padding-bottom: ${props => props.theme.contentPaddingSmall}px;
-  margin: 0 auto;
-  max-width: 800px;
+  max-width: ${props =>
+    props.theme.pageContentMaxWidth + 2 * grid.sideMargin}px;
+  margin-left: auto;
+  margin-right: auto;
+
+  padding-bottom: ${4 * baselineSmall}px;
 
   @media (min-width: ${baselineBreakpoint}px) {
-    padding-left: ${props => props.theme.contentPaddingLarge}px;
-    padding-right: ${props => props.theme.contentPaddingLarge}px;
-    padding-bottom: ${props => props.theme.contentPaddingLarge}px;
-  }
-`;
-
-const Crumb = styled(Breadcrumb)`
-  margin-top: ${3 * baselineSmall}px;
-
-  @media (min-width: ${baselineBreakpoint}px) {
-    margin-top: ${9 * baselineLarge}px;
+    padding-bottom: ${4 * baselineLarge}px;
   }
 `;
 
 const Heading = styled(H1)`
-  margin-top: ${3 * baselineSmall}px;
+  margin-top: ${7 * baselineSmall}px;
+  margin-left: ${props => props.theme.contentPaddingSmall}px;
+  margin-right: ${props => props.theme.contentPaddingSmall}px;
 
   @media (min-width: ${baselineBreakpoint}px) {
-    margin-top: ${3 * baselineLarge}px;
+    font-size: 48px;
+    line-height: 64px;
+    letter-spacing: -1.2px;
+
+    margin-top: ${14 * baselineLarge}px;
+    margin-left: ${props =>
+      props.theme.contentPaddingLarge + grid.sideMargin}px;
+    margin-right: ${props =>
+      props.theme.contentPaddingLarge + grid.sideMargin}px;
+
+    // Offset baseline
+    top: -1px;
   }
 `;
 
 const Description = styled(P)`
   margin-top: ${3 * baselineSmall}px;
+  margin-left: ${props => props.theme.contentPaddingSmall}px;
+  margin-right: ${props => props.theme.contentPaddingSmall}px;
 
   @media (min-width: ${baselineBreakpoint}px) {
-    margin-top: ${3 * baselineLarge}px;
-  }
-`;
-
-const Separator = styled(Hr)`
-  margin-top: ${3 * baselineSmall}px;
-
-  @media (min-width: ${baselineBreakpoint}px) {
-    margin-top: ${3 * baselineLarge}px;
+    margin-top: ${2 * baselineLarge}px;
+    margin-left: ${props =>
+      props.theme.contentPaddingLarge + grid.sideMargin}px;
+    margin-right: ${props =>
+      props.theme.contentPaddingLarge + grid.sideMargin}px;
   }
 `;
 
 const Index = styled(ArticleIndex)`
-  margin-top: ${3 * baselineSmall}px;
+  margin-top: ${7 * baselineSmall}px;
+  margin-left: ${grid.smallGap}px;
+  margin-right: ${grid.smallGap}px;
 
   @media (min-width: ${baselineBreakpoint}px) {
-    margin-top: ${3 * baselineLarge}px;
+    margin-top: ${9 * baselineLarge}px;
   }
 `;
 
@@ -77,15 +81,8 @@ const ArticleIndexPage = props => {
       activeCategory={category}
     >
       <Content>
-        <Crumb
-          links={[
-            { path: '/', label: 'Docs' },
-            { path: `/${category}/`, label: title },
-          ]}
-        />
         <Heading>{title}</Heading>
         <Description>{description}</Description>
-        <Separator />
         <Index pathPrefix={`/${category}/`} articles={articles} />
       </Content>
     </MainLayout>
