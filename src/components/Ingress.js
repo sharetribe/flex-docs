008e39dcc76cf774fe96ca433b7405c966549758
Kimmo Puputti <git@kimmo.kpuputti.fi>
2019-01-31 11:43:54 +0200

    Change Ingress to use H5 styles

--- src/components/Ingress.js
+++ src/components/Ingress.js
@@ -1,43 +1,12 @@
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 
-import fonts from '../fonts';
-import { baselineBreakpoint } from '../config';
-
-const styles = css`
-  // Reset default styles
-  margin: 0;
-
-  // Font
-  ${fonts['CircularStd-Bold'].styles}
-  line-height: 24px;
-  font-size: 18px;
-  letter-spacing: -0.2px;
-
-  // Color
-  color: ${props => props.theme.textColor};
-
-  // Enable baseline offset
-  position: relative;
-
-  // Offset baseline
-  top: 0px;
-
-  @media (min-width: ${baselineBreakpoint}px) {
-    // Font
-    font-size: 24px;
-    line-height: 32px;
-    letter-spacing: -0.2px;
-
-    // Offset baseline
-    top: -1px;
-  }
-`;
+import { H5 } from '../components';
 
 const Ingress = styled.p`
-  ${styles}
+  ${H5.styles}
 `;
 
 // Expose styles
-Ingress.styles = styles;
+Ingress.styles = H5.styles;
 
 export default Ingress;
