69f8838f35f7d7117471b66dbf0bd1df8ab7db9b
Vesa Luusua <vesa.luusua@gmail.com>
2020-02-27 22:58:03 +0200

    Tutorial: enforce article order

--- src/docs/tutorial/order-config.js
+++ src/docs/tutorial/order-config.js
@@ -1,5 +1,3 @@
 // We use slugs since those should be unique
-const sortingArray = [
-  //'install-ftw-daily',
-];
+const sortingArray = ['introduction', 'first-edit', 'change-image-assets', 'change-logo'];
 export default sortingArray;
