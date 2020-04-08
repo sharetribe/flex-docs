225f63fd395d3cdd346be86aec5353bfdcc69eae
Vesa Luusua <vesa.luusua@gmail.com>
2020-04-08 13:12:07 +0300

    remove unused spread-variable

--- src/components/LandingPage/Section.js
+++ src/components/LandingPage/Section.js
@@ -16,7 +16,7 @@ export const SectionHeadingLink = props => {
 };
 
 export const SectionHeading = props => {
-  const { to, children } = props;
+  const { children } = props;
   return <H5 as="h3">{children}</H5>;
 };
 
