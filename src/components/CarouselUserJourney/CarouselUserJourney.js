01d5746e7487e3643b772de706c633d55f8d7e12
Vesa Luusua <vesa.luusua@gmail.com>
2020-04-27 15:25:31 +0300

    Update CarouselContentCreation

--- src/components/CarouselUserJourney/CarouselUserJourney.js
+++ src/components/CarouselUserJourney/CarouselUserJourney.js
@@ -17,14 +17,14 @@ const CarouselUserJourney = props => {
         <h3>Authentication page</h3>
         <p>Users must first login to create listings, or signup to create an account. Saunatime asks for the user's email, first and last name, and password. Signing up accepts the marketplace's terms.</p>
       </Slide>
-      <Slide imgSrc={IM4} imgAlt="asdf">
+      <Slide imgSrc={IMG4} imgAlt="asdf">
         <h3>Listing creation wizard - Description</h3>
         <p>Listings are created through the listing creation wizard. In Saunatime, the wizard includes seven steps. Providers first enter identifying information about the listing: the title and description, as well as searchable fields like the sauna category, price per night, and location. </p>
       </Slide>
       <Slide imgSrc={IMG3} imgAlt="asdf">
         <h3>Listing creation wizard - Availability</h3>
         <p>After sharing the details of their sauna, providers are asked to set the listing's availability calendar.</p>
-        <p>Providers can block unavailable dates in this view. If the provider is editing an already existing listing, they can also see which dates are already booked.</p> 
+        <p>Providers can block unavailable dates in this view. If the provider is editing an already existing listing, they can also see which dates are already booked.</p>
       </Slide>
     </Carousel>
   );
