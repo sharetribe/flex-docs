3397f7c824a3f8d904414459d62654c0ba835c6c
halkaise <44261999+halkaise@users.noreply.github.com>
2020-04-27 17:09:42 +0300

    Added carousel content and pictures, maxwidth to 480

--- src/components/CarouselContentCreation/CarouselContentCreation.js
+++ src/components/CarouselContentCreation/CarouselContentCreation.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { Carousel, CarouselSlide as Slide } from '../../components';
 
-import IMG_LANDING_PAGE from './user-journey-landingpage.png';
+import IMG_LANDING_PAGE from './landing-page.png';
 import IMG_AUTHENTICATION from './authentication-page.png';
-import IMG_WIZARD_AVAILABILITY from './edit-listing-availability.png';
 import IMG_WIZARD_DESCRIPTION from './edit-listing-description.png';
+import IMG_WIZARD_AVAILABILITY from './edit-listing-availability.png';
+import IMG_WIZARD_PHOTOS from './edit-listing-photos.png';
+import IMG_OWN_LISTING from './listing-page-own.png';
+
 
 const CarouselUserJourney = props => {
   return (
-    <Carousel {...props} maxWidth="375px">
+    <Carousel {...props} maxWidth="480px">
       <Slide imgSrc={IMG_LANDING_PAGE} imgAlt="asdf">
         <h3>Landing page</h3>
         <p>Providers in the Saunatime marketplace list their saunas through a listing creation wizard. The wizard is accessed from the top bar's "Add your sauna" call to action. </p>
@@ -25,6 +28,14 @@ const CarouselUserJourney = props => {
         <h3>Listing creation wizard - Availability</h3>
         <p>After sharing the details of their sauna, providers are asked to set the listing's availability calendar.</p>
         <p>Providers can block unavailable dates in this view. If the provider is editing an already existing listing, they can also see which dates are already booked.</p>
+      </Slide>
+      <Slide imgSrc={IMG_WIZARD_PHOTOS} imgAlt="asdf">
+        <h3>Listing creation wizard - Photos and payout details</h3>
+        <p>The final step to publishing a listing in Saunatime is uploading photos. Providers must also enter their bank account information in their user profile to process online payments. Once their account is verified by Stripe, providers can process payment card payments and receive payouts on the marketplace.</p>
+      </Slide>
+      <Slide imgSrc={IMG_WIZARD_AVAILABILITY} imgAlt="asdf">
+        <h3>Own listing</h3>
+        <p>A user can access their published and draft listings from "My listings" in their profile. The listing page displays the information they entered. They can edit the listing following a link on top of the listing image. </p>
       </Slide>
     </Carousel>
   );
