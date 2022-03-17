f05b47ae04679c02b0a248baee2779ceea246491
Aleksi O'Connor <aleksi@sharetribe.com>
2022-03-17 10:04:39 +0200

    run yarn format

--- src/components/Video.js
+++ src/components/Video.js
@@ -5,30 +5,30 @@ import styled from 'styled-components';
  * Video component for embedding video
  */
 const VideoTag = styled.video`
-width: 100%;
-height: auto;
-max-width: 720px; // Match max-width of text paragraphs
-margin-bottom: 24px;
-`
+  width: 100%;
+  height: auto;
+  max-width: 720px; // Match max-width of text paragraphs
+  margin-bottom: 24px;
+`;
 
-const VideoElement = ({children}) => {
-    const mapChildren = React.Children.map(children, child => {
-        return typeof(child) !== 'string' ? React.cloneElement(child) : null;
-    })
-    return (
-        <VideoTag controls muted>
-        {children && mapChildren}
-        <p>Your browser does not support embedded videos</p>
-        </VideoTag>
-    )
-}
+const VideoElement = ({ children }) => {
+  const mapChildren = React.Children.map(children, child => {
+    return typeof child !== 'string' ? React.cloneElement(child) : null;
+  });
+  return (
+    <VideoTag controls muted>
+      {children && mapChildren}
+      <p>Your browser does not support embedded videos</p>
+    </VideoTag>
+  );
+};
 
- const Video = props => {
-    return (
-        <div>
-            <VideoElement children={props.children} />
-        </div>
-    )
-  };
+const Video = props => {
+  return (
+    <div>
+      <VideoElement children={props.children} />
+    </div>
+  );
+};
 
-  export default Video;
+export default Video;
