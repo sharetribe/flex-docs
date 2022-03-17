876af60751d5333b526c2c1b57ce8fabd1b97902
Aleksi O'Connor <aleksi@sharetribe.com>
2022-03-17 09:52:33 +0200

    add responsive styling and fallback

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,16 +1,25 @@
 import React from 'react';
+import styled from 'styled-components';
 
 /**
- * Video component
+ * Video component for embedding video
  */
+const VideoTag = styled.video`
+width: 100%;
+height: auto;
+max-width: 720px; // Match max-width of text paragraphs
+margin-bottom: 24px;
+`
+
 const VideoElement = ({children}) => {
     const mapChildren = React.Children.map(children, child => {
         return typeof(child) !== 'string' ? React.cloneElement(child) : null;
     })
     return (
-        <video controls height="220" width="390">
+        <VideoTag controls muted>
         {children && mapChildren}
-        </video>
+        <p>Your browser does not support embedded videos</p>
+        </VideoTag>
     )
 }
 
