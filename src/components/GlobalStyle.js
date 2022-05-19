51358df54d7c19c485f00fa788622ba6ecc2755c
Aleksi O'Connor <aleksi@sharetribe.com>
2022-05-19 16:47:10 +0300

    css specificity

--- src/components/GlobalStyle.js
+++ src/components/GlobalStyle.js
@@ -17,74 +17,77 @@ const GlobalStyle = createGlobalStyle`
     font-family: "Circular", Helvetica, Arial, sans-serif;
   }
 
-  .DocSearch-Button-Container svg {
-    height: 15px;
-  }
-
-  .DocSearch-MagnifierLabel {
-    color: ${props => props.theme.searchResultsSecondaryColor};
-  }
-
-  .DocSearch-Prefill {
-    color: ${props => props.theme.searchResultsHighlightColor};
-  }
-  .DocSearch-Form {
-    box-shadow: inset 0 0 0 1px ${props => props.theme.searchInputBorderColor};
-  }
-
-  .DocSearch-Cancel {
-    color: ${props => props.theme.searchResultsSecondaryColor};
-  }
-
-  .DocSearch-Button {
+  .DocSearch.DocSearch-Button {
     background-color: #f9f9f9;
+    &:hover {
+      box-shadow: inset 0 0 0 1px ${props => props.theme.searchInputBorderColor};
+    }
   }
 
-  .DocSearch-Button:hover {
-    box-shadow: inset 0 0 0 1px ${props => props.theme.searchInputBorderColor};
-  }
+  .DocSearch {
 
-  .DocSearch-Dropdown {
-    overflow-x: hidden;
-    padding: 0;
-  }
+    .DocSearch-Button-Container svg {
+      height: 15px;
+    }
 
-  .DocSearch-MagnifierLabel svg {
-    height: 18px;
-  }
+    .DocSearch-MagnifierLabel {
+      color: ${props => props.theme.searchResultsSecondaryColor};
+    }
 
-  .DocSearch-Hit {
-    padding-bottom: 0;
-    border-radius: 0;
+    .DocSearch-Prefill {
+      color: ${props => props.theme.searchResultsHighlightColor};
+    }
+    .DocSearch-Form {
+      box-shadow: inset 0 0 0 1px ${props => props.theme.searchInputBorderColor};
+    }
 
-    border-bottom: 1px solid ${props => props.theme.searchInputBorderColor};
-  }
+    .DocSearch-Cancel {
+      color: ${props => props.theme.searchResultsSecondaryColor};
+    }
 
-  .DocSearch-Hits mark {
-    color: ${props => props.theme.searchResultsHighlightColor};
-  }
 
-  .DocSearch-Hit-icon {
-    height: 27px;
-    width: 20px;
-  }
+    .DocSearch-Dropdown {
+      overflow-x: hidden;
+      padding: 0;
+    }
 
-  .DocSearch-Hit a {
-    border-radius: 0;
-    box-shadow: none;
-  }
+    .DocSearch-MagnifierLabel svg {
+      height: 18px;
+    }
 
-  .DocSearch-Hit[aria-selected=true] a {
-    background-color: ${props => props.theme.searchResultsSecondaryColor};
-  }
+    .DocSearch-Hit {
+      padding-bottom: 0;
+      border-radius: 0;
 
-  .DocSearch-Hit-source {
-    padding-left: 18px;
-    padding-bottom: 9px;
-    margin-bottom: 1px;
-    box-shadow: 0px 2px 4px 0px rgb(126 126 126 / 19%);
-    color: ${props => props.theme.searchResultsSecondaryColor};
-  }
+      border-bottom: 1px solid ${props => props.theme.searchInputBorderColor};
+    }
+
+    .DocSearch-Hits mark {
+      color: ${props => props.theme.searchResultsHighlightColor};
+    }
+
+    .DocSearch-Hit-icon {
+      height: 27px;
+      width: 20px;
+    }
+
+    .DocSearch-Hit a {
+      border-radius: 0;
+      box-shadow: none;
+    }
+
+    .DocSearch-Hit[aria-selected=true] a {
+      background-color: ${props => props.theme.searchResultsSecondaryColor};
+    }
+
+    .DocSearch-Hit-source {
+      padding-left: 18px;
+      padding-bottom: 9px;
+      margin-bottom: 1px;
+      box-shadow: 0px 2px 4px 0px rgb(126 126 126 / 19%);
+      color: ${props => props.theme.searchResultsSecondaryColor};
+    }
+}
 
   a#CybotCookiebotDialogPoweredbyCybot,
   div#CybotCookiebotDialogPoweredByText {
